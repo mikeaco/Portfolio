54c02cea76ae84f7226978d1e828cefb4abe585b
mikeaco <michael.acolatse@gmail.com>
2023-06-08 15:07:02 -0400

    redesign

--- portfolio.js
+++ portfolio.js
@@ -1,42 +1,42 @@
 
-var scroll = window.requestAnimationFrame ||
-             function(callback){ window.setTimeout(callback, 1000/60)};
-var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+// var scroll = window.requestAnimationFrame ||
+//              function(callback){ window.setTimeout(callback, 1000/60)};
+// var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
 
-function loop() {
-    //console.log(elementsToShow);
-    Array.prototype.forEach.call(elementsToShow, function(element){
+// function loop() {
+//     //console.log(elementsToShow);
+//     Array.prototype.forEach.call(elementsToShow, function(element){
         
-      if (isElementInViewport(element)) {
-        //console.log("ji");
-        element.classList.add('is-visible');
-      } else {
-        element.classList.remove('is-visible');
-      }
-    });
+//       if (isElementInViewport(element)) {
+//         //console.log("ji");
+//         element.classList.add('is-visible');
+//       } else {
+//         element.classList.remove('is-visible');
+//       }
+//     });
 
-    scroll(loop);
-}
+//     scroll(loop);
+// }
 
-// Call the loop for the first time
-loop();
+// // Call the loop for the first time
+// loop();
 
-// Helper function from: http://stackoverflow.com/a/7557433/274826
-function isElementInViewport(el) {
-  // special bonus for those using jQuery
-  //console.log("hi");
-  if (typeof jQuery === "function" && el instanceof jQuery) {
-    el = el[0];
-  }
-  var rect = el.getBoundingClientRect();
-  return (
-    (rect.top <= 0
-      && rect.bottom >= 0)
-    ||
-    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-    ||
-    (rect.top >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-  );
-}
+// // Helper function from: http://stackoverflow.com/a/7557433/274826
+// function isElementInViewport(el) {
+//   // special bonus for those using jQuery
+//   //console.log("hi");
+//   if (typeof jQuery === "function" && el instanceof jQuery) {
+//     el = el[0];
+//   }
+//   var rect = el.getBoundingClientRect();
+//   return (
+//     (rect.top <= 0
+//       && rect.bottom >= 0)
+//     ||
+//     (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+//       rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+//     ||
+//     (rect.top >= 0 &&
+//       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+//   );
+// }
